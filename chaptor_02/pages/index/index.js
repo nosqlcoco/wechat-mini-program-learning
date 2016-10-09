9261dab348d9ed19dbfbb853babe9af8656aa2f8
Shanqiang Ke <nosqlcoco@gmail.com>
2016-10-09 23:36:37 +0800

    事件绑定及手势控制

--- chaptor_02/pages/index/index.js
+++ chaptor_02/pages/index/index.js
@@ -3,6 +3,7 @@
 var app = getApp()
 Page({
   data: {
+    btnWidth: 200,
     touchLog:"",
     lastTapDiffTime: 0,
     startPoint: [0,0],
@@ -24,11 +25,12 @@ Page({
       })
     })
   },
+  //判断单击还是双击
   mytap: function(e){
-    //this.setData({touchLog:this.data.touchLog + "\r" + e.timeStamp + '- tap'});
     var curTime = e.timeStamp;
     var lastTime = this.data.lastTapDiffTime;
     if(lastTime > 0){
+      //如果两次单击间隔小于700毫秒，认为是双击
       if(curTime - lastTime < 700){
         console.log(e.timeStamp + '- db tap')
       }else{
@@ -39,15 +41,18 @@ Page({
     }
     this.setData({lastTapDiffTime: curTime});
   },
+  //识别长按
   mylongtap: function(e){
     console.log(e.timeStamp + '- long tap')
   },
+  //触摸开始
   mytouchstart: function(e){
     console.log(e.timeStamp + '- touch start')
     this.setData({startPoint: [e.touches[0].pageX, e.touches[0].pageY]});
   },
+  //触摸点移动
   mytouchmove: function(e){
-    
+    //当前触摸点坐标
     var curPoint = [e.touches[0].pageX,e.touches[0].pageY];
     var startPoint = this.data.startPoint;
     if(curPoint[0] <= this.data.startPoint[0]){
@@ -55,8 +60,10 @@ Page({
         console.log(e.timeStamp + '- touch left move')
       }else{
         if(curPoint[1]>= startPoint[1]){
+          this.setData({btnWidth: this.data.btnWidth-1})
           console.log(e.timeStamp + '- touch down move')
         }else{
+          this.setData({btnWidth: this.data.btnWidth+1})
           console.log(e.timeStamp + '- touch up move')
         }
       }
@@ -65,17 +72,21 @@ Page({
         console.log(e.timeStamp + '- touch right move')
       }else{
         if(curPoint[1]>= startPoint[1]){
+          this.setData({btnWidth: this.data.btnWidth-1})
           console.log(e.timeStamp + '- touch down move')
         }else{
+          this.setData({btnWidth: this.data.btnWidth+1})
           console.log(e.timeStamp + '- touch up move')
         }
       }
     }
   },
+  //触摸被中断
   mytouchcacel: function(e){
     console.log(e.timeStamp + '- touch cancel')
     
   },
+  //触摸结束
   mytouchend: function(e){
     console.log(e.timeStamp + '- touch end')
     
