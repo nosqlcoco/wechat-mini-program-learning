bb2e1707a383ad17ba577d252282f1af50172188
Shanqiang Ke <nosqlcoco@gmail.com>
2016-10-09 23:25:38 +0800

    时间及手势控制

--- chaptor_02/pages/index/index.js
+++ chaptor_02/pages/index/index.js
@@ -3,8 +3,10 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello 小程序',
-    userInfo: {}
+    touchLog:"",
+    lastTapDiffTime: 0,
+    startPoint: [0,0],
+    endPoint: [0,0]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -13,7 +15,6 @@ Page({
     })
   },
   onLoad: function () {
-    console.log('onLoad')
     var that = this
   	//调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
@@ -23,17 +24,60 @@ Page({
       })
     })
   },
-  //
+  mytap: function(e){
+    //this.setData({touchLog:this.data.touchLog + "\r" + e.timeStamp + '- tap'});
+    var curTime = e.timeStamp;
+    var lastTime = this.data.lastTapDiffTime;
+    if(lastTime > 0){
+      if(curTime - lastTime < 700){
+        console.log(e.timeStamp + '- db tap')
+      }else{
+        console.log(e.timeStamp + '- tap')
+      }
+    }else{
+      console.log(e.timeStamp + '- first tap')
+    }
+    this.setData({lastTapDiffTime: curTime});
+  },
+  mylongtap: function(e){
+    console.log(e.timeStamp + '- long tap')
+  },
   mytouchstart: function(e){
-    console.log('touch start')
+    console.log(e.timeStamp + '- touch start')
+    this.setData({startPoint: [e.touches[0].pageX, e.touches[0].pageY]});
   },
   mytouchmove: function(e){
-    console.log('touch move')
+    
+    var curPoint = [e.touches[0].pageX,e.touches[0].pageY];
+    var startPoint = this.data.startPoint;
+    if(curPoint[0] <= this.data.startPoint[0]){
+      if(Math.abs(curPoint[0]-startPoint[0])>= Math.abs(curPoint[1]-startPoint[1])){
+        console.log(e.timeStamp + '- touch left move')
+      }else{
+        if(curPoint[1]>= startPoint[1]){
+          console.log(e.timeStamp + '- touch down move')
+        }else{
+          console.log(e.timeStamp + '- touch up move')
+        }
+      }
+    }else{
+      if(Math.abs(curPoint[0]-startPoint[0])>= Math.abs(curPoint[1]-startPoint[1])){
+        console.log(e.timeStamp + '- touch right move')
+      }else{
+        if(curPoint[1]>= startPoint[1]){
+          console.log(e.timeStamp + '- touch down move')
+        }else{
+          console.log(e.timeStamp + '- touch up move')
+        }
+      }
+    }
   },
   mytouchcacel: function(e){
-    console.log('touch cancel')
+    console.log(e.timeStamp + '- touch cancel')
+    
   },
   mytouchend: function(e){
-    console.log('touch end')
+    console.log(e.timeStamp + '- touch end')
+    
   }
 })
