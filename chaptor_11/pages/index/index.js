083d147dd4d7598b187ad812b05ca5887156dc6a
Shanqiang Ke <nosqlcoco@gmail.com>
2016-12-22 00:15:23 +0800

    小程序分享 扫码 置顶 客服消息

---
+++ chaptor_11/pages/index/index.js
@@ -0,0 +1,47 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  onShareAppMessage: function(){
+    return {
+      title: 'Hello World',
+      desc: '小程序分享',
+      path: '/pages/share/share?id=123'
+    }
+  },
+  scanqr: function(e){
+    wx.scanCode({
+      success: function(res){
+        // success
+        console.log(res)
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  }
+})
